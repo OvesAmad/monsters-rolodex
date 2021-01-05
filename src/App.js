555ce3f6be12b97673042a362e07299bcd4ebc56
Oves Amad <oves023@gmail.com>
2021-01-05 12:04:12 +0000

    monsters rolodex update to css and components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: [
-        {
-          id: 1,
-          name: 'Frankenstein'
-        },
-        {
-          id: 2,
-          name: 'Dracula'
-        },
-        {
-          id: 3,
-          name: 'Zombie'
-        }
-      ]
+      monsters: [],
+      searchField: ''
     }
   }
 
+  componentDidMount() {
+    // Fetch gets us the users and then .json is used to convert the response into something more readable
+    fetch('https://jsonplaceholder.typicode.com/users').then(response => response.json()).then(users =>
+      // Lets now update the states monsters property using the inbuilt setState function
+      this.setState({ monsters: users }));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) => 
+    monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => <h1 key={monster.id}> {monster.name} </h1>)
-        }
+      <h1>Monsters Rolodex</h1>
+      <SearchBox
+        placeholder='search monsters'
+        handleChange={this.handleChange}
+      />
+        <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
   }
